ef0574f87c9aebd6fd94dfcd226df70411eb936e
Jon <markwujiahao@gmail.com>
2022-03-28 17:20:49 +0800

    Mar. 28th, 2022

--- word_tool.py
+++ word_tool.py
@@ -1,7 +1,29 @@
 if __name__ == '__main__':
-    from docxtpl import DocxTemplate
+    from docxtpl import DocxTemplate,RichText
 
     doc = DocxTemplate("5HG035877_CNS3.0.docx")
-    context = {'partnumbername': "World company"}
+    rt = RichText('World company', italic = True)
+    rt.add("Star", color='#ff00ff', style="Times New Roman")
+    context = {'partnumbername': rt}
+
     doc.render(context)
     doc.save("generated_doc.docx")
+
+
+
+    """
+    from docxtpl import DocxTemplate, RichText
+    
+    tpl = DocxTemplate('test_files/richtext_tpl.docx')
+    
+    rt = RichText('an exemple of ')
+    rt.add('a rich text', style='myrichtextstyle')
+    rt.add('some violet', color='#ff00ff')
+    
+    context = {
+        'example': rt,
+    }
+    
+    tpl.render(context)
+    tpl.save('test_files/richtext.docx')
+    """
